4599f67231dd529befda254eea3367c211873ea5
papuht <papuht@utu.fi>
2022-02-27 12:02:05 +0200

    Table Members added

---
+++ Tables/dbo.Members.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Members] (
+  [Id] [int] IDENTITY,
+  [FirstName] [nvarchar](255) NOT NULL,
+  [LastName] [nvarchar](255) NOT NULL,
+  [AddressKey] [int] NOT NULL,
+  [BirthDate] [smalldatetime] NOT NULL,
+  [SSn] [int] NOT NULL,
+  CONSTRAINT [PK_Members] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+GO
